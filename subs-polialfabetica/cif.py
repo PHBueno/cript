a98be375be701d9bdbe47a891b9a1804a5dd9ff7
pedro <contato0113@gmail.com>
2023-07-07 00:12:36 -0300

    [feat] - Adiciona código para cifragem de bloco

--- subs-polialfabetica/cif.py
+++ subs-polialfabetica/cif.py
@@ -50,7 +50,7 @@ def ocorrencias(msg: str):
 
 
 # Realiza as substituições com base na regra estabelecida para criptografar
-def substituicoes(msg: str):
+def cif(msg: str):
     oc = ocorrencias(msg)
     _msg = msg
     s1 = tabela_subs(alfabeto_subs=alfabeto_subs_1, alfabeto_originario=alfabeto)
@@ -75,11 +75,43 @@ def substituicoes(msg: str):
     return _msg
 
 
+def dcif(msg: str):
+    oc = ocorrencias(msg)
+    _msg = msg
+    s1 = tabela_subs(alfabeto_subs=alfabeto, alfabeto_originario=alfabeto_subs_1)
+    s2 = tabela_subs(alfabeto_subs=alfabeto, alfabeto_originario=alfabeto_subs_2)
+    for l in oc:
+        cont = 0
+        quantidade = oc[l]
+        while True:
+            if cont >= len(quantidade):
+                break
+            if cont == 0:
+                _msg = _msg[:quantidade[0]] + s1[l] + _msg[quantidade[0] + 1:]
+            if len(quantidade) >= 2:
+                _msg = _msg[:quantidade[1]] + s2[l] + _msg[quantidade[1] + 1:]
+                del quantidade[0:2]
+                cont = 0
+                if len(quantidade) >= 1:
+                    _msg = _msg[:quantidade[0]] + l + _msg[quantidade[0] + 1:]
+                    del quantidade[0]
+            else:
+                cont += 1
+    return _msg
+
+
 if __name__ == '__main__':
     print(banner)
+    escolha = int(input("Escolha [1 - Cifrar; 2 - Decifrar]: "))
     msg = str(input("Mensagem: ")).upper()
     msg = msg.replace(" ", "-")
 
-    print('-=' * len(msg))
-    print(f"Cifra: {substituicoes(msg=msg)}")
-    print('-=' * len(msg))
+    if escolha == 1:
+        print('-=' * len(msg))
+        print(f"Cifra: {cif(msg=msg)}")
+        print('-=' * len(msg))
+    elif escolha == 2:
+        print('-=' * len(msg))
+        print(ocorrencias(msg))
+        print(f"Decifra: {dcif(msg=msg)}")
+        print('-=' * len(msg))
